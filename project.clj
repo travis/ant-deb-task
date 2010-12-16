8e5f627f2f11992dd6f149de26217f02db177364
Travis Vachon <travis.vachon@gmail.com>
2010-12-16 10:19:33 -0500

    Add leiningen project.clj and gitignore
    This lets us build a maven-compatible package for ant-deb-task.
    
    Choose 0.1.1 to differentiate between upstream version for now.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject tvachon/ant-deb-task "0.1.1"
+  :description "An ant task for building debs - upstream source here:
+
+http://code.google.com/p/ant-deb-task/"
+  :java-source-path "src")
